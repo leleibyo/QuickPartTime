2055b624173b9db6d6d58f912b9da41033f15433
leleibyo <43334919+leleibyo@users.noreply.github.com>
2018-09-17 17:00:54 +0800

    Update fontSIze.js

--- quickPT/js/fontSIze.js
+++ quickPT/js/fontSIze.js
@@ -1,18 +1,105 @@
-(function (doc, win) {
-    var docEl = doc.documentElement;
-    var metaEl = doc.querySelector('meta[name="viewport"]');
-    var dpr = win.devicePixelRatio || 1;
-    var scale = 1 / dpr;
-    // 设置viewport，进行缩放，达到高清效果
-    metaEl.setAttribute('content', 'width=' + dpr * docEl.clientWidth + ',initial-scale=' + scale + ',maximum-scale=' + scale + ', minimum-scale=' + scale + ',user-scalable=no,viewport-fit=cover');
-    resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
-    recalc = function () {
-       var clientWidth = docEl.clientWidth;
-       if (!clientWidth) return;
-       docEl.style.fontSize = clientWidth / (750 / 75) + 'px';      
-   };
+var url='http://www.qq.com/';
+var options={
+statusbar: {
+color: '#ffffff'
+},
+toolbar: {
+height: 50,
+color: '#ffffff'
+},
+backButton: {
+image: 'back',
+imagePressed: 'back',
+align: 'right',
+event: 'backPressed'
+},
+forwardButton: {
+image: 'forward',
+imagePressed: 'forward',
+align: 'right',
+event: 'forwardPressed'
+},
+closeButton: {
+image: 'close',
+imagePressed: 'close',
+align: 'right',
+event: 'closePressed'
+},
+customButtons: [
+                {
+                image: 'home',
+                imagePressed: 'home',
+                align: 'right',
+                event: 'refreshPressed'
+                },
+                {
+                image: 'refresh',
+                imagePressed: 'refresh',
+                align: 'right',
+                event: 'homePressed'
+                }
+                ],
+backButtonCanClose: false,
+disableAnimation: true
+};
 
-    if (!doc.addEventListener) return;
-    win.addEventListener(resizeEvt, recalc, false);
-    doc.addEventListener('DOMContentLoaded', recalc, false);
-})(document, window);
+var methods={
+    getOffLine:function (){
+        var oDiv=document.createElement('div');
+        oDiv.id='offline-div'
+        oDiv.innerHTML='<img src="./static/images/sigo.png"><p>network failure</p><button>retry</button>';
+        document.body.appendChild(oDiv);
+    },
+    removeOffline:function (){
+        document.body.style.display="none"
+
+        var oDiv=document.getElementById('offline-div');
+        if(oDiv){
+            document.body.removeChild(oDiv);
+        } 
+    },
+    initMain:function(){
+        document.addEventListener('deviceready', function(){
+            if(url!=''){
+                cordova.ThemeableBrowser.open(url,'_0',options)
+            }
+        }, false);
+    }
+};
+
+if (navigator.onLine) {
+    methods.removeOffline();
+    methods.initMain();
+    //console.log("正常工作！");
+
+} else {
+    //执行离线状态时的任务
+    methods.getOffLine();
+    //console.log("离线工作！");
+
+};
+
+var EventUtil = {
+    addHandler: function(element, type, handler) {
+        if (element.addEventListener) {
+            element.addEventListener(type, handler, false);
+        } else if (element.attachEvent) {
+            element.attachEvent("on" + type, handler);
+        } else {
+            element["on" + type] = handler;
+        }
+    }
+};
+
+EventUtil.addHandler(window, "online", function() {
+    //alert("Online---正常工作");
+    methods.removeOffline();
+    methods.initMain();
+});
+EventUtil.addHandler(window, "offline", function() {
+    //alert("Offline ---离线工作");
+    methods.getOffLine();
+});
+
+
+
